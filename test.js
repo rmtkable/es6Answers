9e1f3a07101481e0d6797b3cf3165e1fb070df2f
Austin <Harrenaustint@gmail.com>
2021-07-15 10:49:21 -0400

    first 16

---
+++ test.js
@@ -0,0 +1,7 @@
+import { uppercaseString } from './index';
+
+// const sum = (...args) => {
+//     const args = [x, y, z];
+//     return args.reduce((a, b) => a + b, 0);
+//   }
+  console.log(uppercaseString('alllowercase'));
